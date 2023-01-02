dac539d0142ac93537bba78fdaf5a80e0fd094a2
Karniela <095282kitty@gmail.com>
2023-01-02 13:05:01 +0800

    .graphql complete

--- backend/models/painting.js
+++ backend/models/painting.js
@@ -2,6 +2,7 @@ import mongoose from 'mongoose'
 const Schema = mongoose.Schema
 const PaintingSchema = new Schema({
     title: { type: String, required: true},
+    painter:{ type: String, required: true},
     material:{ type: String, required: true},
     dimension:{ type: String, required: true},
     price:{ type: Number, required: true},
