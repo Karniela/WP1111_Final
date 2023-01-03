688de324466d1ee99b154720d7d2eafb88f925b0
Tania Liao <TaniaLiao@MacBook-Pro-642.local>
2023-01-03 16:43:00 +0800

    Nobody knows how to use github

--- backend/src/models/painting.js
+++ backend/src/models/painting.js
@@ -1,6 +1,7 @@
 import mongoose from 'mongoose'
 const Schema = mongoose.Schema
 const PaintingSchema = new Schema({
+    imgsrc: { type: String, required: true },
     title: { type: String, required: true},
     painter:{ type: String, required: true},
     material:{ type: String, required: true},
